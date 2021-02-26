7df59a06ea0a9111b9d5854884b484e375c90ad1
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-02-26 12:20:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages';
+import { Switch, Route } from 'react-router-dom'
+import About from './pages/about';
+import Menu from './pages/menu';
+import Dropdown from './components/Dropdown';
+
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', hideMenu)
+
+    return () => {
+      window.removeEventListener('resize', hideMenu)
+    }
+  })
+
+  return (
+    <>
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/about" component={About} />
+      </Switch>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="flex justify-center items-center bg-yellow-300 h-screen">
+      <h1 className="text-9xl uppercase font-black">About Page</h1>
+    </div>
+  )
+}
+
+export default About
